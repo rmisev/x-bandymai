cefb0a58f261bc519217aff6ae11da1adcd93fcf
Rimas Misevičius <19497908+rmisev@users.noreply.github.com>
2017-09-15 21:30:40 +0300

    Add cpp code

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argv, char* argc[]) {
+  std::cout << "Sveiki, čia labai gera progrma.\n";
+  std::cout << "Tik deja ji nieko naudingo nedaro.\n";
+  
+  return 0;
+}
